605c0b01469bb7d69d47b2484a5b63d86d447341
jjax <jax1994@gmail.com>
2016-09-18 12:03:59 +0200

    Complete last commit with missing files

---
+++ src/Enums/RequestStatus.h
@@ -0,0 +1,17 @@
+/*
+ * ActorType.h
+ *
+ *  Created on: Jul 2, 2016
+ *      Author: jjax
+ */
+
+#ifndef REQUESTSTATUS_H_
+#define REQUESTSTATUS_H_
+
+enum RequestStatus{
+	ACCEPT,
+	REFUSE,
+	WAIT
+};
+
+#endif /* REQUESTSTATUS_H_ */
